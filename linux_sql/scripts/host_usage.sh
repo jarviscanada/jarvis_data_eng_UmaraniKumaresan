50d65f32215e8093448886c2fdb7f1086271a9ea
Umarani Kumaresan <umarani.kumaresan@gmail.com>
2024-09-13 18:00:36 +0000

    host usage tested

--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -23,14 +23,16 @@ memory_free=$(echo "$vmstat_mb" | awk '{print $4}'| tail -n1 | xargs)
 cpu_idle=$(echo "$vmstat_mb" |awk '{print $15}'| tail -1 | xargs) 
 cpu_kernel=$(echo "$vmstat_mb" | awk '{print $14}'| tail -1 | xargs)
 disk_io=$(vmstat -d | awk '{print $10}' | tail -1 | xargs)
-disk_available=$(df -BM | awk '{print $4 - "M"}' | tail -1 | xarg)
+disk_available=$(df -BM | awk '{print $4 - "M"}' | tail -1 | xargs)
 
 timestamp=$(date +'%Y-%m-%d %T')
 
-host_id="SELECT id FROM host_info WHERE hostname='$hostname'";
 
-insert_stmt="INSERT INTO host_usage (timestamp, host_id, memory_free, cpu_idel, cpu_kernel, disk_io, disk_available)
- VALUES('$timestamp' , '$host_id', '$memory_free', '$cpu_idle', '$cpu_kernel', '$disk_io', '$disk_available')";
+
+host_id="(SELECT id FROM host_info WHERE hostname='$hostname')";
+
+insert_stmt="INSERT INTO host_usage (timestamp, host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available)
+ VALUES('$timestamp' , $host_id, '$memory_free', '$cpu_idle', '$cpu_kernel', '$disk_io', '$disk_available')";
 
 export PGPASSWORD=$psql_password
 
