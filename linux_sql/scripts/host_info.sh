09b7a9f8e6e63cdba98cc831bec65a8a874330b8
Umarani Kumaresan <umarani.kumaresan@gmail.com>
2024-09-14 15:04:13 +0000

    tested host_info

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -15,14 +15,14 @@ hostname=$(hostname -f)
 cpu_number=$(lscpu  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
 cpu_architecture=$(lscpu | grep Architecture | awk '{print $2}' | xargs)
 cpu_model=$(lscpu | grep "Model name" | awk -F: '{print $2}' | xargs)
-cpu_mhz=$(lscpu | grep "CPU MHz" | awk -F: '{print $2}' | xargs)
 l2_cache=$(lscpu | grep "L2 cache" | awk -F : '{print $2 - "K"}' | xargs)
+cpu_mhz=$(cat /proc/cpuinfo | egrep "^cpu MHz" | head -n 1 | awk '{print $4}' | xargs)
 timestamp=$(date +'%Y-%m-%d %T')
 total_mem=$(free -h | awk '/Mem/{print $2 - "G"}')
 
 
 insert_stmt="INSERT INTO host_info ( hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, "l2_cache", "timestamp", "total_mem")
-VALUES( '$hostname', "$cpu_number", '$cpu_architecture', '$cpu_model',0, "$l2_cache", '$timestamp', "$total_mem")"
+VALUES( '$hostname', "$cpu_number", '$cpu_architecture', '$cpu_model','$cpu_mhz', "$l2_cache", '$timestamp', "$total_mem")"
 
 export PGPASSWORD=$psql_password
 
