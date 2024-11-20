62360d648bd8cc034b734380f35dc36136cd4b8c
Umarani Kumaresan <umarani.kumaresan@gmail.com>
2024-11-20 02:07:18 +0000

    grep

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImpEnhanced.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImpEnhanced.java
@@ -11,7 +11,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class JavaGrepImp implements JavaGrep {
+public class JavaGrepImpEnhanced implements JavaGrep {
     final static Logger logger = LoggerFactory.getLogger(JavaGrepImp.class);
 
     private String regex;
