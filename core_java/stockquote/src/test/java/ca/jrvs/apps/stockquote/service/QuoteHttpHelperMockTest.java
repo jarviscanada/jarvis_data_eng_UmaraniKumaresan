e8016ad94a8aded293fd9cbed69d61c6497c3ade
Umarani Kumaresan <umarani.kumaresan@gmail.com>
2024-12-03 02:09:00 +0000

    stockquote initial

--- core_java/stockquote/src/test/java/ca/jrvs/apps/stockquote/service/QuoteHttpHelperMockTest.java
+++ core_java/stockquote/src/test/java/ca/jrvs/apps/stockquote/service/QuoteHttpHelperMockTest.java
@@ -1,55 +1,66 @@
 package ca.jrvs.apps.stockquote.service;
 
+import ca.jrvs.apps.stockquote.DTO.GlobalQuoteDTO;
 import ca.jrvs.apps.stockquote.DTO.Quote;
-import junit.framework.TestCase;
-import net.bytebuddy.agent.VirtualMachine;
-import org.junit.runner.Request;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.Spy;
-import org.mockito.junit.MockitoJUnitRunner;
+import ca.jrvs.apps.stockquote.util.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.mockito.Mockito;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
-public class QuoteHttpHelperMockTest extends TestCase {
-    private QuoteHttpHelper quoteHttpHelper;
-    private HttpClient mockClient;
-    private OkHttpClient client;
-    private VirtualMachine.ForHotSpot.Connection.Response mockResponse;
-    private ResponseBody mockResponseBody;
+public class QuoteHttpHelperMockTest {
 
-    @Mock
-    HttpResponse<InputStream> mockResponseHttpResponse;
-    @RunWith(MockitoJUnitRunner.class)
+    @Test
+    public void testFetchQuoteInfo() throws Exception {
+        // Arrange
+        String mockSymbol = "MSFT";
+        String mockResponseBody = "{\n" +
+                "  \"Global Quote\": {\n" +
+                "    \"01. symbol\": \"MSFT\",\n" +
+                "    \"02. open\": \"330.00\",\n" +
+                "    \"03. high\": \"335.00\",\n" +
+                "    \"04. low\": \"328.00\",\n" +
+                "    \"05. price\": \"332.50\",\n" +
+                "    \"06. volume\": \"15000000\",\n" +
+                "    \"07. latest trading day\": \"2024-12-01\",\n" +
+                "    \"08. previous close\": \"331.00\",\n" +
+                "    \"09. change\": \"1.50\",\n" +
+                "    \"10. change percent\": \"0.45%\"\n" +
+                "  }\n" +
+                "}";
 
-    @Spy
-    HttpClient httpClient;
+        // Mocking HttpClient and HttpResponse
+        HttpClient mockHttpClient = Mockito.mock(HttpClient.class);
+        HttpResponse<String> mockHttpResponse = Mockito.mock(HttpResponse.class);
 
-    public void test() throws Exception {
-        when(httpClient.send(any(), any(HttpResponse.BodyHandlers.ofInputStream().getClass())))
-                .thenReturn(mockResponseHttpResponse);
+        // Setting up mock behavior
+        when(mockHttpResponse.body()).thenReturn(mockResponseBody);
+        when(mockHttpClient.send(any(HttpRequest.class), any(HttpResponse.BodyHandler.class)))
+                .thenReturn(mockHttpResponse);
 
-    HttpClient client = HttpClient.newHttpClient();
+        // Creating a QuoteHttpHelper instance with the mocked HttpClient
+        QuoteHttpHelper quoteHttpHelper = new QuoteHttpHelper(mockHttpClient);
 
-    @Mock
-    public void setUp() throws Exception {
+        // Act
+        Quote result = quoteHttpHelper.fetchQuoteInfo(mockSymbol);
 
-        mockClient = mock(HttpClient.class);
-        mockResponse = mock(VirtualMachine.ForHotSpot.Connection.Response.class);
-        mockResponseBody = mock(ResponseBody.class);
-        HttpClient mockClient = mock(HttpClient.class, withSettings().useConstructor().defaultAnswer(CALLS_REAL_METHODS));
+        // Assert
+        assertEquals("MSFT", result.getTicker());
+        assertEquals(330.00, result.getOpen(), 0.01);
+        assertEquals(335.00, result.getHigh(), 0.01);
+        assertEquals(328.00, result.getLow(), 0.01);
+        assertEquals(332.50, result.getPrice(), 0.01);
+        assertEquals(15000000, result.getVolume());
+        assertEquals(331.00, result.getPreviousClose(), 0.01);
+        assertEquals(1.50, result.getChange(), 0.01);
+        assertEquals("0.45%", result.getChangePercent());
 
-
-    }
-
-    public void tearDown() throws Exception {
-    }
-
-    public void testFetchQuoteInfo() {
     }
 }
